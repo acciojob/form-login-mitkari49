8f81efd6de8ad9850b123dbf6b5bae8cc392da5b
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-08-27 23:23:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+
 function getFormvalue() {
     //Write your code here
-	let name = document.queryselector="text"
-	console.log(name);
-
+	let FirstName = document.querySelector("input[name='fname']")
+	let LastName = document.querySelector("input[name='lname']")
+    const fname=FirstName.value;
+    const lname=LastName.value;
+    alert(fname+" "+lname);
 }
+
